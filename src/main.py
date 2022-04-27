5c891cc1885ca84b803b78ec1fee426c644b9c75
Juliette <juliette28@hotmail.co.uk>
2022-04-27 08:35:57 +0100

    Added comments

--- src/main.py
+++ src/main.py
@@ -5,12 +5,13 @@ from reducer import *
 from shuffle import *
 from functools import reduce
 def cls():
-    os.system("cls" if os.name == "nt" else "clear")
+    os.system("cls" if os.name == "nt" else "clear") # Clear the terminal for cleaner coding
 
 def main():
 
     cls()
 
+    #Load in the data file
     df = pd.read_csv("data/AComp_Passenger_data_no_error.csv", names=[
                 "passenger_id",
                 "flight_id",
@@ -19,27 +20,36 @@ def main():
                 "departure_time",
                 "flight_duration",
             ])
-
+    #Preprocessing - remove duplicate rows
     df = df.drop_duplicates()
 
     ## Task 1
 
+    #Map in is a list of [flight_id, from_airport]
     map_in = df.loc[:,["flight_id", "from_airport"]].values.tolist()
     reduce_out = {}
+    #Map each list item into a tuple with '1'
     map_out = map(map_func, map_in)
+    #Shuffle the map_out to give a dictionary instead with just the from_airport
     reduce_in = shuffle_flights(map_out)
+    #Reduce to the sum of each key in the dictionary
     for key, values in reduce_in.items():
         reduce_out[key] = reduce(reduce_func, values)
     print("Task 1")
     print(reduce_out)
 
     ## Task 2
+    #Map in a list of passenger_id
     map_in = df.loc[:,"passenger_id"].values.tolist()
     reduce_out = {}
+    #Map each passenger_id into a tuple with '1'
     map_out = map(map_func, map_in)
+    #Shuffle the map out to give a dictionary
     reduce_in = shuffle_passengers(map_out)
+    #Reduce to the sum of each key in the dictionary
     for key, values in reduce_in.items():
         reduce_out[key] = reduce(reduce_func, values)
+    #Find the maximum
     max_keys = [[k,v] for k, v in reduce_out.items() if v == max(reduce_out.values())]
     print("Task 2")
     print(max_keys)
--- src/shuffle.py
+++ src/shuffle.py
@@ -1,6 +1,7 @@
 def shuffle_flights(mapper_out):
     data = {}
     flight_ids = []
+    #For each [flight_id, from_airport] key in mapper_out, if we haven't counted the fight yet, then add it into the dictionary
     for k, v in mapper_out:
         if k[0] not in flight_ids:
             flight_ids.append(k[0])
@@ -12,6 +13,7 @@ def shuffle_flights(mapper_out):
 
 def shuffle_passengers(mapper_out):
     data = {}
+    #For each passenger_id key in mapper_out, if we haven't counted the passenger flight yet, then set the value, otherwise append to the values
     for k, v in mapper_out:
         if k not in data:
             data[k] = [v]
